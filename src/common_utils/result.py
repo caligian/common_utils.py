6d9672d7e55a9ed52ba95369bf492a002d1d6dd5
caligian <caligian@protonmail.com>
2026-03-18 16:03:59 +0530

    TODO: check result API

--- src/common_utils/result.py
+++ src/common_utils/result.py
@@ -10,9 +10,20 @@ Error = TypeVar("Error", bound=Exception)
 class Result(Generic[T]):
     __match_args__ = ("value", "metadata")
 
-    def __init__(self, value: T, metadata: dict[str, any] | None = None) -> None:
+    def __init__(
+        self,
+        value: T,
+        metadata: dict[str, any] | None = None,
+        result_type: str = "Success",
+    ) -> None:
         self.value = value
         self.metadata: dict[str, any] = {} if not metadata else metadata
+        self.type: str = result_type
+
+        if isinstance(self.value, Exception):
+            self.type = "Failure"
+        else:
+            self.type = "Success"
 
     def __str__(self) -> str:
         s = str(self.value)
@@ -62,65 +73,17 @@ class Result(Generic[T]):
             else:
                 return value
 
-    def unwrap(self, pcall: bool = False) -> any:
+    def err_args(self) -> tuple | None:
         if self.not_ok():
-            return self.throw(pcall=pcall)
-        else:
-            return self.value
+            return self.value.args
 
-    def merge_metadata(self, result: Self) -> Self:
-        metadata = result.metadata
-        for k, v in self.metadata.items():
-            if metadata.get(k) is None:
-                metadata[k] = v
-        return result
+    def args(self) -> tuple | None:
+        return self.err_args()
 
-    def map(self, f: Callable[[any], any], *args, **kwargs) -> Self:
-        cls = type(self)
-        try:
-            res = f(self.value, *args, **kwargs)
-            if isinstance(res, cls):
-                return self.merge_metadata(res)
-            else:
-                return cls(res, self.metadata)
-        except Exception as error:
-            return cls(error, self.metadata)
+    def error_args(self) -> tuple | None:
+        return self.err_args()
 
-    def unwrap_or(
-        self,
-        f: Callable[[Exception], any],
-        or_else: Callable[[T], any] = lambda x: x,
-    ) -> Self[Error] | Self[T]:
-        if self.err():
-            return self.map(f)
-        else:
-            return self.map(or_else)
-
-    def unwrap_and(
-        self,
-        f: Callable,
-        or_else: Callable[[Error], any] = lambda error: error,
-    ) -> Self[Error] | Self[T]:
-        if self.ok():
-            return self.map(f)
-        else:
-            return self.map(or_else)
-
-    def bind(self, f: Callable) -> None:
-        self.apply.append(f)
-
-
-class Failure(Result[Error]):
-    __match_args__ = ("value", "metadata")
-
-    def __init__(self, value: Error, metadata: dict[str, any] | None = None) -> None:
-        assert isinstance(value, Exception)
-        super().__init__(value, metadata)
-
-    def args(self) -> tuple:
-        return self.value.args
-
-    def message(
+    def err_message(
         self,
         tostr: bool = False,
         format_with_metadata: bool = True,
@@ -134,13 +97,34 @@ class Failure(Result[Error]):
                 s = args[0].format(*self.metadata)
             else:
                 s = args[0]
-        elif isinstance(args[0], (list, bytes)) and len(args) == 1:
+        elif len(args) > 0 and isinstance(args[0], (str, bytes)) and len(args) == 1:
             if format_with_metadata:
                 s = args[0].format(*self.metadata)
             else:
                 s = args[0]
 
         return s
+
+    def error_message(
+        self,
+        tostr: bool = False,
+        format_with_metadata: bool = True,
+    ) -> str | None:
+        return self.err_message(tostr, format_with_metadata)
+
+    def msg(
+        self,
+        tostr: bool = False,
+        format_with_metadata: bool = True,
+    ) -> str | None:
+        return self.err_message(tostr, format_with_metadata)
+
+    def message(
+        self,
+        tostr: bool = False,
+        format_with_metadata: bool = True,
+    ) -> str | None:
+        return self.err_message(tostr, format_with_metadata)
 
     def print(
         self,
@@ -155,13 +139,66 @@ class Failure(Result[Error]):
         msg = self.message(tostr=tostr, format_with_metadata=format_with_metadata)
         cprint(msg, color)
 
+    def unwrap(self, pcall: bool = False) -> any:
+        if self.not_ok():
+            return self.throw(pcall=pcall)
+        else:
+            return self.value
+
+    def merge_metadata(self, result: Self) -> Self:
+        metadata = result.metadata
+        for k, v in self.metadata.items():
+            if metadata.get(k) is None:
+                metadata[k] = v
+        return result
+
+    def map(self, f: Callable[[any], "Result"], *args, **kwargs) -> Self:
+        cls = type(self)
+        if cls.__name__ != "Result":
+            cls = self.__base__
+
+        try:
+            res = f(self.value, *args, **kwargs)
+            if isinstance(res, cls):
+                res = self.merge_metadata(res)
+                res.type = "Success"
+                return res
+            else:
+                return cls(res, self.metadata, "Success")
+        except Exception as error:
+            return cls(error, self.metadata, "Failure")
+
+    def unwrap_or(
+        self,
+        default: any = None,
+        default_factory: Callable[[Error], any] = lambda err: None,
+    ) -> Self[Error] | Self[T]:
+        if self.err():
+            if default is not None:
+                return default
+            elif default_factory and callable(default_factory):
+                return default_factory(self.value)
+        else:
+            return self.value
+
+    def cbind(self, f: Callable) -> None:
+        self.apply.append(f)
+
+
+class Failure(Result[Error]):
+    __match_args__ = ("value", "metadata")
+
+    def __init__(self, value: Error, metadata: dict[str, any] | None = None) -> None:
+        assert isinstance(value, Exception)
+        super().__init__(value, metadata, "Failure")
+
 
 class Success(Result[T]):
     __match_args__ = ("value", "metadata")
 
     def __init__(self, value: T, metadata: dict[str, any] | None = None) -> None:
         assert not isinstance(value, Exception)
-        super().__init__(value, metadata)
+        super().__init__(value, metadata, "Success")
 
 
 def safe(f: Callable) -> Callable[[...], Success | Failure]:
@@ -182,4 +219,44 @@ def safe(f: Callable) -> Callable[[...], Success | Failure]:
     return function
 
 
-__all__ = ["Success", "Failure", "safe", "Result", "Error", "T"]
+def is_result(obj: Result) -> bool:
+    return isinstance(obj, Result)
+
+
+def is_ok(obj: Result) -> bool:
+    return isinstance(obj, Success) or is_result(obj) and obj.type == "Success"
+
+
+def is_err(obj: Result) -> bool:
+    return isinstance(obj, Failure) or is_result(obj) and obj.type == "Failure"
+
+
+def unwrap(
+    obj: Result,
+    f: Callable | None = None,
+    default: any = None,
+    default_factory: Callable | None = None,
+    pcall: bool = False,
+) -> any:
+    if is_ok(obj):
+        return obj.unwrap()
+    elif default is not None:
+        return default
+    elif default_factory is not None:
+        return default_factory()
+
+
+AnyResult = Success | Failure
+__all__ = [
+    "Success",
+    "Failure",
+    "Result",
+    "AnyResult",
+    "Error",
+    "T",
+    "safe",
+    "is_ok",
+    "is_err",
+    "is_result",
+    "unwrap",
+]
